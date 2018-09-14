e8accf03e67ae9e5f32a66fc144fed3c9e1898ed
Vitor Oliveira <vbrazo@gmail.com>
2018-09-13 22:42:17 -0300

    Add User field and query types file

---
+++ application/api/graph/fields/user_field.rb
@@ -0,0 +1,9 @@
+UserField = GraphQL::Field.define do
+  name 'User'
+  description 'Single user'
+  type UserType
+
+  argument :id, types.Int
+
+  resolve -> (_obj, args, _ctx) { Api::Operations::User::Show.new(args) }
+end
