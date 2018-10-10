af4a65276db17a932e9b22cea0d0e6e5964cd62d
Vitor Oliveira <vbrazo@gmail.com>
2018-10-10 13:09:52 -0300

    Add napicer_skill foreign_key and minor changes

--- application/api/routes/main.rb
+++ application/api/routes/main.rb
@@ -9,7 +9,7 @@ class ApiSupport
         r.post do
           params = format_params
 
-          create_user_operation = Api::Operations::User::Create.new(params['query'])
+          create_user_operation = Api::Operations::User::Create.new(user: params['query'])
           result = create_user_operation.create
 
           result.to_json
--- application/db/migrations/20180930172908_create_napicers_table.rb
+++ application/db/migrations/20180930172908_create_napicers_table.rb
@@ -5,6 +5,35 @@ Sequel.migration do
       String :first_name, null: false
       String :last_name, null: false
       String :username, null: false
+      String :avatar
+      String :status
+      String :nickname, null: false
+      String :availability
+      String :desired_hourly_rate
+      String :self_description
+      String :mailing_address_1
+      String :mailing_address_2
+      String :phone
+      String :timezone
+      String :city
+      String :state_or_province
+      String :postal_code
+      String :country
+      String :skype
+      String :twitter
+      String :github_username
+      String :linkedin_url
+      String :current_employment_situation
+      String :data_science
+      String :development
+      String :devops
+      String :design
+      String :management
+      String :storage
+      String :others
+      String :introduction
+      String :nationality
+      String :position
       DateTime :created_at
       DateTime :updated_at
     end
