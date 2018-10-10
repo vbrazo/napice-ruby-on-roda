0dbb50406ae146a08087c7b638e51f89a7b6b178
Vitor Oliveira <vbrazo@gmail.com>
2018-10-10 14:06:32 -0300

    Minor changes on factories

--- spec/factories/napicer_education.rb
+++ spec/factories/napicer_education.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :napicer_education, class: Api::Models::NapicerEducation do
-    school { Faker::Lorem.word }
-    degree { Faker::Lorem.word }
+    school { Faker::Educator.university }
+    degree { Faker::Educator.degree }
     self_description { Faker::Lorem.word }
 
     napicer
